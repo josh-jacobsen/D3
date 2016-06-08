56a21560c76100db63f975ec16616277f27c97ca
EDA <eda@mac-mini-3.local>
2016-06-08 17:00:24 +1200

    first commit

---
+++ parser.js
@@ -0,0 +1,35 @@
+import fs from 'fs'
+
+// template
+//
+// fs.readFile('./output/test2.json', (err, data) => {
+//   if (err) throw err;
+//   let test = JSON.parse(data)
+//   let num = test.formImage.Pages[0].Texts.length
+//   let totalText = ''
+//   for (let i = 0; i < test.formImage.Pages[0].Texts.length; i++) {
+//     let text = test.formImage.Pages[0].Texts[i].R[0].T;
+//     totalText += text
+//   }
+//   let decoded = (decodeURI(totalText))
+//   fs.writeFile('./output/budget', decoded, (err) => {
+//     if (err) throw err;
+//   })
+//
+// })
+
+fs.readFile('./output/test2.json', (err, data) => {
+  if (err) throw err;
+  let test = JSON.parse(data)
+  let num = test.formImage.Pages[0].Texts.length
+  let totalText = ''
+  for (let i = 0; i < test.formImage.Pages[0].Texts.length; i++) {
+    let text = test.formImage.Pages[0].Texts[i].R[0].T;
+    totalText += text
+  }
+  let decoded = (decodeURI(totalText))
+  fs.writeFile('./output/budget', decoded, (err) => {
+    if (err) throw err;
+  })
+
+})
