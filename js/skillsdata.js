a1f3185cb9b70741da8f20cb30944946d565bffd
Please osxKeychain please let me be free <eda@mac-mini-3.local>
2016-06-16 15:06:00 +1200

    create sunburst chart

---
+++ js/skillsdata.js
@@ -0,0 +1,104 @@
+var skillsdata;
+
+skillsdata = {
+  "Skills": {
+    "Army": {
+      "SOF": {
+        "Personell": [0, 0, 0, 43, 45],
+        "Operating": [0, 0, 0, 23, 26],
+        "Depreciation": [0, 0, 0, 6, 7],
+        "Capital Charge": [0, 0, 0, 8, 9],
+      },
+      "Land CSS": {
+        "Personell": [0, 0, 0, 79, 82],
+        "Operating": [0, 0, 0, 45, 47],
+        "Depreciation": [0, 0, 0, 18, 17],
+        "Capital Charge": [0, 0, 0, 25, 24]
+      },
+      "Land Combat Support": {
+        "Personell": [0, 0, 0, 104, 103],
+        "Operating": [0, 0, 0, 55, 60],
+        "Depreciation": [0, 0, 0, 22, 21],
+        "Capital Charge": [0, 0, 0, 30, 30]
+      },
+      "Land Combat": {
+        "Personell": [0, 0, 0, 149, 153],
+        "Operating": [0, 0, 0, 91, 98],
+        "Depreciation": [0, 0, 0, 41, 39],
+        "Capital Charge": [0, 0, 0, 54, 53]
+      }
+    },
+    "Navy": {
+      "Helicopters": {
+        "Personell": [0, 0, 0, 26, 27],
+        "Operating": [0, 0, 0, 22, 26],
+        "Depreciation": [0, 0, 0, 18, 15],
+        "Capital Charge": [0, 0, 0, 22, 17],
+      },
+      "Patrol Forces": {
+        "Personell": [0, 0, 0, 47, 47],
+        "Operating": [0, 0, 0, 22, 24],
+        "Depreciation": [0, 0, 0, 20, 19],
+        "Capital Charge": [0, 0, 0, 34, 26]
+      },
+      "Littoral Warfare": {
+        "Personell": [0, 0, 0, 15, 14],
+        "Operating": [0, 0, 0, 9, 6],
+        "Depreciation": [0, 0, 0, 2, 3],
+        "Capital Charge": [0, 0, 0, 6, 6]
+      },
+      "Naval Support": {
+        "Personell": [0, 0, 0, 39, 40],
+        "Operating": [0, 0, 0, 25, 24],
+        "Depreciation": [0, 0, 0, 13, 21],
+        "Capital Charge": [0, 0, 0, 21, 23]
+      },
+      "Naval Combat": {
+        "Personell": [0, 0, 0, 94, 99],
+        "Operating": [0, 0, 0, 68, 59],
+        "Depreciation": [0, 0, 0, 74, 86],
+        "Capital Charge": [0, 0, 0, 96, 83]
+      }
+    },
+    "Air Force": {
+      "Rotary Transport": {
+        "Personell": [0, 0, 0, 62, 63],
+        "Operating": [0, 0, 0, 38, 41],
+        "Depreciation": [0, 0, 0, 38, 43],
+        "Capital Charge": [0, 0, 0, 78, 78],
+      },
+      "Fixed Transport": {
+        "Personell": [0, 0, 0, 80, 82],
+        "Operating": [0, 0, 0, 78, 88],
+        "Depreciation": [0, 0, 0, 47, 43],
+        "Capital Charge": [0, 0, 0, 46, 49]
+      },
+      "Surveillance": {
+        "Personell": [0, 0, 0, 61, 62],
+        "Operating": [0, 0, 0, 52, 57],
+        "Depreciation": [0, 0, 0, 32, 38],
+        "Capital Charge": [0, 0, 0, 32, 45]
+      }
+    },
+    "Joint": {
+      "Policy": {
+        "Personell": [0, 0, 0, 12, 8],
+        "Operating": [0, 0, 0, 3, 3],
+        "Depreciation": [0, 0, 0, 1, 1],
+        "Capital Charge": [0, 0, 0, 1, 1]
+      },
+      "Support": {
+        "Personell": [0, 0, 0, 15, 17],
+        "Operating": [0, 0, 0, 8, 8],
+        "Depreciation": [0, 0, 0, 1, 1],
+        "Capital Charge": [0, 0, 0, 1, 2]
+      },
+      "Deployed Forces": {
+        "Personell": [0, 0, 0, 14, 4],
+        "Operating": [0, 0, 0, 29, 9],
+        "Depreciation": [0, 0, 0, 1, 1],
+        "Capital Charge": [0, 0, 0, 1, 2]
+      }
+    }
+  }
+};
